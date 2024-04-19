0baf7315bb9a024a15221714c73abd8528e20995
Ahmed Nasr <work@ahmednasr.at>
2024-04-19 09:33:16 +0200

    day12 hide api_key using dotenv

--- day12-query-dotenv/index.js
+++ day12-query-dotenv/index.js
@@ -1,10 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
+
 import { MoviesAPI } from "./nichtUnserCode/fake_moviedatabase.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/movies", (req, res) => {
-  MoviesAPI("1239248C12628D1AB117A8A1")
+  MoviesAPI(process.env.API_KEY)
     .then((movies) => res.json(movies))
     .catch((err) => res.status(500).json(err));
 });
