741201c98dc185aa224173054bdde9fbe74c5c82
Stefan Osorio <stefan@osorio.de>
2024-06-13 12:58:15 +0200

    plantshop: useRef wiederholung

--- day_38-plantshop/frontend/src/pages/LoginPage.tsx
+++ day_38-plantshop/frontend/src/pages/LoginPage.tsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { postlogin } from "../utils/api";
 import { useShopState } from "../zustand";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const {user, setUser} = useShopState()
+  const {setUser} = useShopState()
   const navigate = useNavigate()
-
-  console.log({user})
+  const emailInput = useRef()
+  const passwordInput = useRef()
 
 
   const submitLogin = async () => {
+    const email = emailInput.current.value
+    const password = passwordInput.current.value
     const {user} = await postlogin({email, password}).json() 
     setUser(user)
     navigate('/')
-
   };
 
   return (
@@ -27,10 +26,7 @@ const LoginPage = () => {
         <input
           type="text"
           name="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
+          ref={emailInput}
         />
       </label>
       <br />
@@ -39,10 +35,7 @@ const LoginPage = () => {
         <input
           type="password"
           name="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
+          ref={passwordInput}
         />
       </label>
       <br />
@@ -54,3 +47,57 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
+
+
+// const LoginPage = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const {user, setUser} = useShopState()
+//   const navigate = useNavigate()
+
+//   console.log({user})
+
+
+//   const submitLogin = async () => {
+//     const {user} = await postlogin({email, password}).json() 
+//     setUser(user)
+//     navigate('/')
+
+//   };
+
+//   return (
+
+//     <form>
+//       <label>
+//         Email
+//         <input
+//           type="text"
+//           name="email"
+//           value={email}
+//           onChange={(e) => {
+//             setEmail(e.target.value);
+//           }}
+//         />
+//       </label>
+//       <br />
+//       <label>
+//         Password
+//         <input
+//           type="password"
+//           name="password"
+//           value={password}
+//           onChange={(e) => {
+//             setPassword(e.target.value);
+//           }}
+//         />
+//       </label>
+//       <br />
+//       <button type="button" onClick={submitLogin}>
+//         Login!
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default LoginPage;
